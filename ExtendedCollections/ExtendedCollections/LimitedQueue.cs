f5777d042f567cedef1de68e7c09698d074854e7
Odonno <bottiau.david@laposte.net>
2019-10-29 14:26:10 +0100

    chore: first version

---
+++ ExtendedCollections/ExtendedCollections/LimitedQueue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtendedCollections
+{
+    public class LimitedQueue<T>
+    {
+        private readonly ConcurrentQueue<T> _queue = new ConcurrentQueue<T>();
+
+        /// <summary>
+        /// Limit of the queue, meaning max number of items in the queue.
+        /// </summary>
+        public int Limit { get; }
+
+        /// <summary>
+        /// Number of items in the queue.
+        /// </summary>
+        public int Count => _queue.Count;
+
+        /// <summary>
+        /// Whether there is no item in the queue.
+        /// </summary>
+        public bool IsEmpty => _queue.IsEmpty;
+
+        /// <summary>
+        /// Values inside the queue.
+        /// </summary>
+        public List<T> Values => _queue.ToList();
+
+        /// <summary>
+        /// Event triggered when an element is enqueued.
+        /// </summary>
+        public event EventHandler Enqueued;
+
+        /// <summary>
+        /// Event triggered when an element is dequeued.
+        /// </summary>
+        public event EventHandler Dequeued;
+
+        public LimitedQueue()
+        {
+            Enqueued += HandleEnqueued;
+        }
+
+        private void HandleEnqueued(object sender, EventArgs e)
+        {
+            while (Count > Limit)
+            {
+                TryDequeue();
+            }
+        }
+
+        /// <summary>
+        /// Enqueue a new item.
+        /// </summary>
+        /// <param name="item">Item to enqueue.</param>
+        public void Enqueue(T item)
+        {
+            _queue.Enqueue(item);
+            Enqueued?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Dequeue an item (if possible).
+        /// </summary>
+        /// <returns>Returns whether the dequeue has been done, with the element dequeued.</returns>
+        public Tuple<bool, T> TryDequeue()
+        {
+            bool success = _queue.TryDequeue(out var item);
+
+            if (success)
+            {
+                Dequeued?.Invoke(this, EventArgs.Empty);
+            }
+
+            return Tuple.Create(success, item);
+        }
+    }
+}
---
+++ ExtendedCollections/ExtendedCollections/LimitedStack.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtendedCollections
+{
+    public class LimitedStack<T>
+    {
+        private readonly ConcurrentStack<T> _stack = new ConcurrentStack<T>();
+
+        /// <summary>
+        /// Limit of the stack, meaning max number of items in the stack.
+        /// </summary>
+        public int Limit { get; }
+
+        /// <summary>
+        /// Number of items in the stack.
+        /// </summary>
+        public int Count => _stack.Count;
+
+        /// <summary>
+        /// Whether there is no item in the stack.
+        /// </summary>
+        public bool IsEmpty => _stack.IsEmpty;
+
+        /// <summary>
+        /// Values inside the stack.
+        /// </summary>
+        public List<T> Values => _stack.ToList();
+
+        /// <summary>
+        /// Event triggered when an element is pushed.
+        /// </summary>
+        public event EventHandler Pushed;
+
+        /// <summary>
+        /// Event triggered when an element is popped.
+        /// </summary>
+        public event EventHandler Popped;
+
+        public LimitedStack()
+        {
+            Pushed += HandlePushed;
+        }
+
+        private void HandlePushed(object sender, EventArgs e)
+        {
+            while (Count > Limit)
+            {
+                TryPop();
+            }
+        }
+
+        /// <summary>
+        /// Push an item in the stack.
+        /// </summary>
+        /// <param name="item">Item to push.</param>
+        public void Push(T item)
+        {
+            _stack.Push(item);
+            Pushed?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Pop an item (if possible).
+        /// </summary>
+        /// <returns></returns>
+        public Tuple<bool, T> TryPop()
+        {
+            bool success = _stack.TryPop(out var item);
+
+            if (success)
+            {
+                Popped?.Invoke(this, EventArgs.Empty);
+            }
+
+            return Tuple.Create(success, item);
+        }
+    }
+}
